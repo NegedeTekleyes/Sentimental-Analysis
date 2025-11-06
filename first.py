a223d625a02ea7b941e5892acc5367bdf59340c0
Negede <negedetekleyes@gmail.com>
2025-11-06 12:40:40 +0300

    Python for developer class

---
+++ first.py
@@ -0,0 +1,5 @@
+database_name = input("What is your database name:")
+table_name = input("What is your table name:")
+cols_num = int(input(" The number of columns in that tables:"))
+# sep is separator to display in new line
+print(f'Database  "{database_name}"" created successfully with a  table named "{table_name}" contaning {cols_num} columns') 
