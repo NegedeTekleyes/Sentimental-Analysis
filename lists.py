243d26b525cea4186910c6a33274fbaf0bd014ee
Negede <negedetekleyes@gmail.com>
2025-11-13 19:37:40 +0300

    exercise answer

--- lists.py
+++ lists.py
@@ -1,19 +1,19 @@
-import sys
+# import sys
+# if len(sys.argv) < 2:
+#     print("Usage: lists.py, database_name, table_name")
+# elif sys.argv[1] == "create_db":
+#     db = []
+#     table_name = input("Enter table name: ")
+#     number_of_cols = int(input("Enter number of cols: "))
 
-if len(sys.argv) < 2:
-    print("Usage: python lists.py create_db table_name")
-elif sys.argv[1] == 'create_db':
-    db = []
-    table_cols = []
-    
-    table_name = input("Enter the table name: ")
-    number_of_cols = int(input("Enter number of columns: "))
-    
-    for i in range(number_of_cols):
-        col_name = input(f"Enter column name for column {i + 1}: ")
-        table_cols.append(col_name)
-    
-    print(f"Table '{table_name}' created with columns: {table_cols}")
 
-else:
-    print('Invalid command')
+
+# define the class nme
+class_name = "Math 101"
+
+# define list of student
+students = ['Negede','charli','cahpe']
+# print class roaster
+print(f'class "{class_name}" roster')
+for student in students:
+    print(f"- {student}")
